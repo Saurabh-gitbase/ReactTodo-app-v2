19828a1766750f2c5bcc5a13c92607f4048e4627
saurabh <rokr.saurabh31@gmail.com>
2020-08-04 12:59:54 +0530

    Updates

---
+++ precache-manifest.309315c99b2c0d922826f56cd75dd912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1c50d3cf0bba967bc9f16a7ac8e358a",
+    "url": "/ReactTodo-app-v2/index.html"
+  },
+  {
+    "revision": "44c53a8f84074fcc11ed",
+    "url": "/ReactTodo-app-v2/static/css/main.dbe35df6.chunk.css"
+  },
+  {
+    "revision": "ff4590739bd95e6c4130",
+    "url": "/ReactTodo-app-v2/static/js/2.8ed3906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTodo-app-v2/static/js/2.8ed3906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44c53a8f84074fcc11ed",
+    "url": "/ReactTodo-app-v2/static/js/main.714ec8c2.chunk.js"
+  },
+  {
+    "revision": "6c049c2b3c442964a6d8",
+    "url": "/ReactTodo-app-v2/static/js/runtime-main.2a60a85b.js"
+  }
+]);
