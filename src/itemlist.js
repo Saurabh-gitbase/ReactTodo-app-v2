dd687e0792ce82b1f69a08e40b82490ebb182c68
saurabh <rokr.saurabh31@gmail.com>
2020-08-05 22:27:21 +0530

    added css for responsive design

--- src/itemlist.js
+++ src/itemlist.js
@@ -17,7 +17,6 @@ function listItem(props){
             )
             }
             </FlipMove>
-            {/* <img src="https://img.icons8.com/fluent/48/000000/delete-sign.png"/> */}
         </div>
         
     )
